7ca449451cf9a8b678de346471c6275c19c11bbf
Laura Ophélie <118055290+lauraophelie@users.noreply.github.com>
2026-05-07 22:55:11 +0300

    some failed test

--- extract.py
+++ extract.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import pytesseract
 
-image = Image.open("./test_samples/test_1.jpg")
+image = Image.open("./test_samples/test_2.jpg")
 gray_image = image.convert("L")
 
 text = pytesseract.image_to_string(gray_image)
