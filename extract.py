a4c8e1420915746011b110b19dfe32199b36d3f6
Laura Ophélie <118055290+lauraophelie@users.noreply.github.com>
2026-05-07 20:55:51 +0300

    simple text extracting using tesseract-ocr

---
+++ extract.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import pytesseract
+
+image = Image.open("./test_samples/test_1.jpg")
+gray_image = image.convert("L")
+
+text = pytesseract.image_to_string(gray_image)
+clean_text = text.replace("\x0c", "").strip()
+print(clean_text)
